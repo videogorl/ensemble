03663d4ee2a5426bf97b226ed363fdb15098f20c
Felicity Reif <felicity@nysics.com>
2026-02-06 12:59:54 -0800

    Remove zIndex from persistent UI layer to fix keyboard avoidance
    - Remove zIndex(2) that was preventing keyboard handling from working
    - Move keyboardAware() to entire ZStack instead of just persistent UI layer
    - Natural ZStack layering should maintain mini player + tab bar on top

--- Packages/EnsembleUI/Sources/Screens/MainTabView.swift
+++ Packages/EnsembleUI/Sources/Screens/MainTabView.swift
@@ -126,12 +126,11 @@ public struct MainTabView: View {
                                     .shadow(color: .black.opacity(0.1), radius: 20, y: -5)
                             )
                     }
-                    #if canImport(UIKit)
-                    .keyboardAware()
-                    #endif
-                    .zIndex(2)
                 }
                 .ignoresSafeArea(.container, edges: .bottom)
+                #if canImport(UIKit)
+                .keyboardAware()
+                #endif
             }
         }
         .sheet(isPresented: $showingNowPlaying) {
