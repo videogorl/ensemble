6f0912dd85fe8d7e72b083e6a3bda3b62eecfc19
Felicity Reif <felicity@nysics.com>
2026-05-06 06:13:47 -0700

    Show macOS album disc headers

--- Packages/EnsembleUI/Sources/Screens/Details/MediaDetailView.swift
+++ Packages/EnsembleUI/Sources/Screens/Details/MediaDetailView.swift
@@ -960,56 +960,101 @@ public struct MediaDetailView<ViewModel: MediaDetailViewModelProtocol>: View {
             nowPlayingVM.play(tracks: viewModel.filteredTracks, startingAt: index)
         }
         #else
-        // macOS: List with native .swipeActions for trackpad two-finger swipe support
-        ForEach(Array(viewModel.filteredTracks.enumerated()), id: \.element.id) { index, track in
-            let resolvedActions = trackInteractionModel.resolve(for: track)
-            VStack(spacing: EnsembleDesign.Spacing.none) {
-                TrackRow(
-                    track: track,
-                    showArtwork: showArtwork,
-                    showTrackNumber: showTrackNumbers,
-                    isPlaying: track.id == currentTrackId,
-                    onPlayNext: resolvedActions.onPlayNext,
-                    onPlayLast: resolvedActions.onPlayLast,
-                    onAddToPlaylist: resolvedActions.onAddToPlaylist,
-                    onAddToRecentPlaylist: resolvedActions.onAddToRecentPlaylist,
-                    onToggleFavorite: resolvedActions.onToggleFavorite,
-                    onGoToAlbum: resolvedActions.onGoToAlbum,
-                    onGoToArtist: resolvedActions.onGoToArtist,
-                    onShareLink: resolvedActions.onShareLink,
-                    onShareFile: resolvedActions.onShareFile,
-                    onRemoveFromPlaylist: playlistTrackRemovalHandler.map { handler in
-                        { handler(track, index) }
-                    },
-                    isFavorited: resolvedActions.isFavorited,
-                    recentPlaylistTitle: resolvedActions.recentPlaylistTitle,
-                    supplementalMetadataWidth: trackListSupplementalMetadataWidth
-                ) {
-                    nowPlayingVM.play(tracks: viewModel.filteredTracks, startingAt: index)
-                }
-                .padding(.horizontal, TrackListLayoutMetrics.rowHorizontalPadding)
-                .padding(.vertical, TrackListLayoutMetrics.rowVerticalPadding)
-
-                if index < viewModel.filteredTracks.count - 1 {
-                    TrackListDivider(
-                        showArtwork: showArtwork,
-                        showTrackNumbers: showTrackNumbers
-                    )
-                }
+        // macOS: SwiftUI List rows retain native trackpad swipe support.
+        // Album details add disc headers using the same multi-disc rule as iOS.
+        ForEach(Array(macDiscTrackGroups.enumerated()), id: \.offset) { _, group in
+            if let disc = group.disc {
+                macDiscHeader(disc)
             }
-            .trackSwipeActions(
-                track: track,
-                nowPlayingVM: nowPlayingVM,
-                onPlayNext: resolvedActions.onPlayNext,
-                onPlayLast: resolvedActions.onPlayLast,
-                onAddToPlaylist: resolvedActions.onAddToPlaylist
-            )
-            .listRowBackground(Color.clear)
-            .listRowInsets(EdgeInsets())
-            .hideListRowSeparator()
+
+            ForEach(group.tracks, id: \.element.id) { indexedTrack in
+                macTrackRow(track: indexedTrack.element, index: indexedTrack.offset)
+            }
         }
         #endif
     }
+
+    #if !os(iOS)
+    private var macDiscTrackGroups: [(disc: Int?, tracks: [(offset: Int, element: Track)])] {
+        let indexedTracks = Array(viewModel.filteredTracks.enumerated())
+        guard groupByDisc else {
+            return [(disc: nil, tracks: indexedTracks)]
+        }
+
+        let grouped = Dictionary(grouping: indexedTracks) { $0.element.discNumber }
+        let sortedDiscs = grouped.keys.sorted()
+        let shouldShowDiscHeaders = sortedDiscs.count > 1
+
+        return sortedDiscs.map { disc in
+            let tracks = grouped[disc]?.sorted { lhs, rhs in
+                lhs.offset < rhs.offset
+            } ?? []
+            return (disc: shouldShowDiscHeaders ? disc : nil, tracks: tracks)
+        }
+    }
+
+    private func macDiscHeader(_ disc: Int) -> some View {
+        Text("Disc \(disc)")
+            .font(.system(size: 15, weight: .semibold))
+            .foregroundColor(EnsembleDesign.Color.secondaryText)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, TrackListLayoutMetrics.rowHorizontalPadding)
+            .padding(.top, EnsembleDesign.Spacing.md)
+            .padding(.bottom, EnsembleDesign.Spacing.xs)
+            .listRowBackground(Color.clear)
+            .listRowInsets(EdgeInsets())
+            .hideListRowSeparator()
+    }
+
+    private func macTrackRow(track: Track, index: Int) -> some View {
+        let resolvedActions = trackInteractionModel.resolve(for: track)
+
+        return VStack(spacing: EnsembleDesign.Spacing.none) {
+            TrackRow(
+                track: track,
+                showArtwork: showArtwork,
+                showTrackNumber: showTrackNumbers,
+                isPlaying: track.id == currentTrackId,
+                onPlayNext: resolvedActions.onPlayNext,
+                onPlayLast: resolvedActions.onPlayLast,
+                onAddToPlaylist: resolvedActions.onAddToPlaylist,
+                onAddToRecentPlaylist: resolvedActions.onAddToRecentPlaylist,
+                onToggleFavorite: resolvedActions.onToggleFavorite,
+                onGoToAlbum: resolvedActions.onGoToAlbum,
+                onGoToArtist: resolvedActions.onGoToArtist,
+                onShareLink: resolvedActions.onShareLink,
+                onShareFile: resolvedActions.onShareFile,
+                onRemoveFromPlaylist: playlistTrackRemovalHandler.map { handler in
+                    { handler(track, index) }
+                },
+                isFavorited: resolvedActions.isFavorited,
+                recentPlaylistTitle: resolvedActions.recentPlaylistTitle,
+                supplementalMetadataWidth: trackListSupplementalMetadataWidth
+            ) {
+                nowPlayingVM.play(tracks: viewModel.filteredTracks, startingAt: index)
+            }
+            .padding(.horizontal, TrackListLayoutMetrics.rowHorizontalPadding)
+            .padding(.vertical, TrackListLayoutMetrics.rowVerticalPadding)
+
+            if index < viewModel.filteredTracks.count - 1 {
+                TrackListDivider(
+                    showArtwork: showArtwork,
+                    showTrackNumbers: showTrackNumbers
+                )
+            }
+        }
+        .trackSwipeActions(
+            track: track,
+            nowPlayingVM: nowPlayingVM,
+            onPlayNext: resolvedActions.onPlayNext,
+            onPlayLast: resolvedActions.onPlayLast,
+            onAddToPlaylist: resolvedActions.onAddToPlaylist
+        )
+        .listRowBackground(Color.clear)
+        .listRowInsets(EdgeInsets())
+        .hideListRowSeparator()
+    }
+    #endif
 
     /// SwiftUI header content embedded as the UITableView's native tableHeaderView.
     /// Scrolls with the track list while preserving cell recycling.
