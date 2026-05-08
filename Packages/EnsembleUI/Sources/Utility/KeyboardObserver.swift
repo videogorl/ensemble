38fa392f28164496ffb9a2e1b51c5d4155b5005d
Felicity Reif <--global>
2026-05-08 11:56:39 -0700

    Remove unused keyboard-aware view helper

--- Packages/EnsembleUI/Sources/Utility/KeyboardObserver.swift
+++ Packages/EnsembleUI/Sources/Utility/KeyboardObserver.swift
@@ -1,4 +1,3 @@
-import SwiftUI
 import Combine
 
 #if canImport(UIKit)
@@ -20,39 +19,4 @@ extension Publishers {
     }
 }
 
-/// Observable object to track keyboard visibility
-public class KeyboardObserver: ObservableObject {
-    @Published public var isVisible = false
-    private var cancellables = Set<AnyCancellable>()
-    
-    public init() {
-        Publishers.keyboardHeight
-            .map { $0 > 0 }
-            .removeDuplicates()
-            .receive(on: RunLoop.main)
-            .assign(to: \.isVisible, on: self)
-            .store(in: &cancellables)
-    }
-}
-
-// View extension to track keyboard height
-extension View {
-    func keyboardAware() -> some View {
-        modifier(KeyboardAwareModifier())
-    }
-}
-
-struct KeyboardAwareModifier: ViewModifier {
-    @State private var keyboardHeight: CGFloat = 0
-    
-    func body(content: Content) -> some View {
-        content
-            .padding(.bottom, keyboardHeight)
-            .onReceive(Publishers.keyboardHeight) { height in
-                withAnimation(.easeOut(duration: 0.25)) {
-                    keyboardHeight = height
-                }
-            }
-    }
-}
 #endif
