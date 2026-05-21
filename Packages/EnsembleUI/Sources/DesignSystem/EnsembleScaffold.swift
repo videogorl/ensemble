ea5deb33f131e0bbfe8d3f0e321907db8bda571c
Felicity Reif <8680903+videogorl@users.noreply.github.com>
2026-05-21 07:21:24 -0700

    Enlarge now playing glass play control

--- Packages/EnsembleUI/Sources/DesignSystem/EnsembleScaffold.swift
+++ Packages/EnsembleUI/Sources/DesignSystem/EnsembleScaffold.swift
@@ -432,8 +432,8 @@ public enum EnsembleScaffold {
         public static let routePickerSize: CGFloat = 24
         public static let primaryControlIconSize: CGFloat = 32
         public static let playPauseControlIconSize: CGFloat = 80
-        public static let playPauseGlassControlSize: CGFloat = 68
-        public static let playPauseGlassIconSize: CGFloat = 34
+        public static let playPauseGlassControlSize: CGFloat = 73
+        public static let playPauseGlassIconSize: CGFloat = 39
         public static let loadingIndicatorScale: CGFloat = 1.5
         public static let emptyIconSize: CGFloat = 48
         public static let emptyArtworkFillOpacity = 0.05
