783ae91975d68493e25046bffdc4e07cb7f47f4e
Felicity <8680903+videogorl@users.noreply.github.com>
2026-09-04 11:06:19 -0700

    Skip Siri authorization on simulator

--- Ensemble/App/AppDelegate+SiriAuthorization.swift
+++ Ensemble/App/AppDelegate+SiriAuthorization.swift
@@ -3,6 +3,10 @@ import Intents
 
 extension AppDelegate {
     func configureSiriAuthorization() {
+        #if targetEnvironment(simulator)
+        AppLogger.debug("AppDelegate: Siri authorization skipped on simulator")
+        return
+        #else
         let status = INPreferences.siriAuthorizationStatus()
         AppLogger.debug("AppDelegate: Siri authorization status at launch: \(status.rawValue)")
 
@@ -13,6 +17,7 @@ extension AppDelegate {
         INPreferences.requestSiriAuthorization { newStatus in
             AppLogger.debug("AppDelegate: Siri authorization prompt result: \(newStatus.rawValue)")
         }
+        #endif
     }
 }
 #endif
